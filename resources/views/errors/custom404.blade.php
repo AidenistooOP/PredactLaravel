42d9184f3e90a8372f3a1a95d4730ae99d0ab55e
Aiden <103739543+AidenistooOP@users.noreply.github.com>
2023-11-14 16:56:24 +0100

    Custom 404 error handling page.

---
+++ resources/views/errors/custom404.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Predact - Home</title>
+    @vite('resources/css/404.css')
+    @vite('resources/js/app.js')
+    <script src="https://kit.fontawesome.com/80bfb3b6e2.js" crossorigin="anonymous"></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6636827498914574"
+       crossorigin="anonymous"></script>    
+</head>
+<body>
+    <div id="mainDiv">
+        <h1 id="title">404 PAGINA NIET GEVONDEN</h1>
+        <a href="/home"><button><i class="fa-solid fa-house"></i> Terug naar home-pagina</button></a>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+</body>
+</html>
